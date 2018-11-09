482a5b65e3df6829859b908dea86e8e856004869
Meghan <nektromc@gmail.com>
2018-11-09 16:53:55 -0500

    add the x-repo custom element

---
+++ x-repo.js
@@ -0,0 +1,30 @@
+//
+"use strict";
+//
+function create_element(name, attrs, children) {
+    var ele = document.createElement(name);
+    (attrs || []).forEach(function(v) { ele.setAttribute(v[0], v[1]); });
+    (children || []).forEach(function(v) { ele.appendChild(v); });
+    return ele;
+}
+function dcTN(string) {
+    return document.createTextNode(string);
+}
+//
+customElements.define("x-repo", class extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        const { id, name, description, uses, website } = this.dataset;
+        this.appendChild(create_element("section", [], [
+            create_element("h3", [], [
+                create_element("span", [], [ dcTN(name ? name : id) ]),
+                create_element("span", [], [ create_element("a", [["href",`https://github.com/nektro/${id}`]], [ dcTN("GitHub") ]) ]),
+                create_element("span", [], [ create_element("a", [["href",(website ? website : `https://nektro.github.io/${id}/`)]], [ dcTN("Website") ]) ]),
+            ]),
+            create_element("p", [], [ dcTN(description) ]),
+            create_element("p", [], [ dcTN(`Built with: ${uses}`) ]),
+        ]));
+    }
+});
