ff2dc8520adb3725c24b348c5c8d692ed4196dd6
Meghan <nektromc@gmail.com>
2019-05-17 22:09:24 -0400

    x-repo : move badges and description to new line

--- x-repo.js
+++ x-repo.js
@@ -22,18 +22,20 @@ customElements.define("x-repo", class extends HTMLElement {
                 create_element("span", [], [ dcTN(name ? name : id) ]),
                 create_element("span", [], [ create_element("a", [["href",`https://github.com/nektro/${id}`]], [ dcTN("GitHub") ]) ]),
                 create_element("span", [], [ create_element("a", [["href",(website ? website : `https://nektro.github.io/${id}/`)]], [ dcTN("Website") ]) ]),
-                create_element("span", [], [ create_element("img", [["src",`https://img.shields.io/github/stars/nektro/${id}.svg?style=flat`]]) ]),
             ]),
-            create_element("p", [], [ dcTN(description) ]),
-            create_element("p", [], [ dcTN(`Built with: ${uses}`) ]),
         ]));
         if (this.dataset.noWebsite !== undefined) {
             this.children[0].children[0].children[2].remove();
         }
         if (this.dataset.noGithub !== undefined) {
-            this.children[0].children[0].children[4].remove();
-            this.children[0].children[0].children[3].remove();
             this.children[0].children[0].children[1].remove();
         }
+        else {
+            this.children[0].appendChild(create_element("h3", [], [
+                create_element("span", [], [ create_element("img", [["src",`https://img.shields.io/github/stars/nektro/${id}.svg?style=flat`]]) ]),
+            ]));
+        }
+        this.children[0].appendChild(create_element("p", [], [ dcTN(description) ]));
+        this.children[0].appendChild(create_element("p", [], [ dcTN(`Built with: ${uses}`) ]));
     }
 });
